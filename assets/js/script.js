1e5adb26601711f8ead4674434c76a68c80a258e
ItsMeHafeez <abdulhafeez272002@gmail.com>
2023-09-28 10:53:18 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+
+
+const express=require("express")
+const mongoose=require("mongoose")
+const body=require("body-parser")
+const app=express()
+app.set('view engine', 'js');
+app.use(body.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+mongoose.connect("mongodb+srv://hafeez:hafeez123@cluster0.ej1noqo.mongodb.net/contact", {useNewUrlParser:true})
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
